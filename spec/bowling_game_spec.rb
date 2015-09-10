eb3d81301647f43f2ffcb5727833cf7f98a80ef4
Jonah Rosenberg <JonahRRosenberg@gmail.com>
2015-09-09 21:21:48 -0500

    Add init game logic with strikes and spares with specs

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -6,16 +6,26 @@ describe Bowling::Game do
     game = Bowling::Game.new
     game.roll(5)
     game.roll(1)
-    game.score.should eq(6)
+    game.score.should eq(5 + 1)
   end
 
-  it "works with a spare" do
+  it "works with a spare and open frame" do
     game = Bowling::Game.new
     game.roll(4)
     game.roll(6)
     game.roll(7)
     game.roll(2)
-    game.score.should eq(26)
+    game.score.should eq(17 + 9)
+  end
+
+  it "works with two spares" do
+    game = Bowling::Game.new
+    game.roll(4)
+    game.roll(6)
+    game.roll(7)
+    game.roll(3)
+    game.roll(2)
+    game.score.should eq(17 + 12)
   end
 
   it "works with a strike" do
@@ -23,6 +33,23 @@ describe Bowling::Game do
     game.roll(10)
     game.roll(1)
     game.roll(7)
-    game.score.should eq(26)
+    game.score.should eq(20 + 8)
+  end
+
+  it "works with a turkey" do
+    game = Bowling::Game.new
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.score.should eq(30)
+  end
+
+  it "works with a four-bagger" do
+    game = Bowling::Game.new
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.roll(10)
+    game.score.should eq(40)
   end
 end
